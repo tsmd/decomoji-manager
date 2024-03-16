7dc40035b54a0d11bfd2bd7bc5b4b890266ccc66
Shimada Takayuki <taka@tsmd.jp>
2024-03-17 00:18:20 +0900

    name must be unique

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,4 +1,13 @@
 class Alias < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 1 }
+  validates :name, presence: true, length: { minimum: 1 }, uniqueness: true
+  validate :unique_name_across_models
   belongs_to :decomoji
+
+  private
+
+  def unique_name_across_models
+    if Decomoji.find_by(name: name)
+      errors.add(:name, 'is already taken')
+    end
+  end
 end
--- app/models/decomoji.rb
+++ app/models/decomoji.rb
@@ -1,7 +1,16 @@
 class Decomoji < ApplicationRecord
   has_many :aliases, dependent: :destroy
 
-  validates :name, presence: true, length: { minimum: 1 }
+  validates :name, presence: true, length: { minimum: 1 }, uniqueness: true
+  validate :unique_name_across_models
   validates :yomi, presence: true, length: { minimum: 1 }
   belongs_to :version
+
+  private
+
+  def unique_name_across_models
+    if Alias.find_by(name: name)
+      errors.add(:name, 'is already taken')
+    end
+  end
 end
