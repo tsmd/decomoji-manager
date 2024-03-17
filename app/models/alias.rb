92851b7a2588203925de3140840b1b02cc2a2c32
Shimada Takayuki <taka@tsmd.jp>
2024-03-17 17:07:40 +0900

    remove alias uniqueness

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,13 +1,4 @@
 class Alias < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 1 }, uniqueness: true
-  validate :unique_name_across_models
+  validates :name, presence: true, length: { minimum: 1 }
   belongs_to :decomoji
-
-  private
-
-  def unique_name_across_models
-    if Decomoji.find_by(name: name)
-      errors.add(:name, 'is already taken')
-    end
-  end
 end
