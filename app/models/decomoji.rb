dc0167def626d56ff71687ab322b66be81c26d31
Shimada Takayuki <taka@tsmd.jp>
2024-03-23 11:39:00 +0900

    output romaji

--- app/models/decomoji.rb
+++ app/models/decomoji.rb
@@ -1,13 +1,15 @@
 class Decomoji < ApplicationRecord
+  include RomajiKanaConverter
+
   validates :name, presence: true, length: { minimum: 1 }, uniqueness: true
   validate :unique_name_across_models
-  
+
   #validates :yomi, presence: true, length: { minimum: 1 }
-  
+
   belongs_to :version
-  
+
   has_many :aliases, dependent: :destroy
-  
+
   has_many :decomoji_tags
   has_many :tags, through: :decomoji_tags
 
@@ -18,6 +20,14 @@ class Decomoji < ApplicationRecord
   before_save :replace_image, if: -> { image.attached? && image.changed? }
 
   belongs_to :color
+
+  def yomi_kunrei
+    yomi.present? ? to_kunrei_romaji(yomi) : ''
+  end
+
+  def yomi_hepburn
+    yomi.present? ? to_hepburn_romaji(yomi) : ''
+  end
 
   private
 
