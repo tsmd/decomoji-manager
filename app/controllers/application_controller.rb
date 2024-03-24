92d15cacb392b066606c1a11a94045b2f2e13478
Shimada Takayuki <taka@tsmd.jp>
2024-03-24 23:12:37 +0900

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with(
+    name: Rails.application.credentials.dig(:basic_auth, :username),
+    password: Rails.application.credentials.dig(:basic_auth, :password),
+  )
 end
