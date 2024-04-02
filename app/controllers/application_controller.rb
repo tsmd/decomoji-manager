227cf9438aba7882032a2e738ff870df818fe23c
Shimada Takayuki <taka@tsmd.jp>
2024-04-03 00:18:44 +0900

    basic auth only when production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with(
-    name: Rails.application.credentials.dig(:basic_auth, :username),
-    password: Rails.application.credentials.dig(:basic_auth, :password),
-  )
+  # production 環境で Basic 認証をかける
+  if Rails.env.production?
+    http_basic_authenticate_with(
+      name: Rails.application.credentials.dig(:basic_auth, :username),
+      password: Rails.application.credentials.dig(:basic_auth, :password),
+    )
+  end
 end
